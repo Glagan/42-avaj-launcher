cddec45fb16093e06cae53d59f20809eee4150fe
Glagan <Glagan@users.noreply.github.com>
2022-09-08 18:27:09 +0200

    Reuse weather for aircrafts on the same coordinates

---
+++ org/glagan/avaj/simulator/WeatherCoordinates.java
@@ -0,0 +1,28 @@
+package org.glagan.avaj.simulator;
+
+public class WeatherCoordinates {
+    private Coordinates coordinates;
+    private String weather;
+
+    public WeatherCoordinates(Coordinates coordinates, String weather) {
+        this.coordinates = coordinates;
+        this.weather = weather;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+}
--- org/glagan/avaj/simulator/WeatherTower.java
+++ org/glagan/avaj/simulator/WeatherTower.java
@@ -1,12 +1,33 @@
 package org.glagan.avaj.simulator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherTower extends Tower {
+    private List<WeatherCoordinates> coordinatesWeather;
+
+    public WeatherTower() {
+        super();
+        this.coordinatesWeather = new ArrayList<WeatherCoordinates>();
+    }
+
     public String getWeather(Coordinates coordinates) {
+        for (WeatherCoordinates weatherCoordinates : coordinatesWeather) {
+            if (weatherCoordinates.getCoordinates().getLongitude() == coordinates.getLongitude() &&
+                    weatherCoordinates.getCoordinates().getLatitude() == coordinates.getLatitude() &&
+                    weatherCoordinates.getCoordinates().getHeight() == coordinates.getHeight()) {
+                return weatherCoordinates.getWeather();
+            }
+        }
+
         WeatherProvider provider = WeatherProvider.getProvider();
-        return provider.getCurrentWeather(coordinates);
+        String weather = provider.getCurrentWeather(coordinates);
+        this.coordinatesWeather.add(new WeatherCoordinates(coordinates, weather));
+        return weather;
     }
 
     public void changeWeather() {
+        this.coordinatesWeather = new ArrayList<WeatherCoordinates>();
         this.conditionsChanged();
     }
 }
